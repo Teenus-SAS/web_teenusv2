d291696b272b390ca64c0e623dec2ea86ec3ab55
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2022-08-11 15:46:47 -0500

    Carga de menu desplegable admin

--- admin/partials/scriptsCSS.php
+++ admin/partials/scriptsCSS.php
@@ -1,7 +1,7 @@
 <!--plugins-->
 <link href="../assets/plugins/vectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet" />
 <link href="../assets/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
-<link href="../assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
+<link href="/admin/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
 <link href="../assets/plugins/metismenu/css/metisMenu.min.css" rel="stylesheet" />
 <!-- loader-->
 <link href="../assets/css/pace.min.css" rel="stylesheet" />
--- admin/partials/scriptsJS.php
+++ admin/partials/scriptsJS.php
@@ -1,9 +1,7 @@
 <!-- JQuery -->
 <script src="../assets/js/jquery.min.js"></script>
-<!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-<!-- <script src="/admin/assets/js/bootstrap.bundle.min.js"></script> -->
 <!--plugins-->
 <script src="../assets/plugins/simplebar/js/simplebar.min.js"></script>
 <script src="/admin/assets/plugins/metismenu/js/metisMenu.min.js"></script>
