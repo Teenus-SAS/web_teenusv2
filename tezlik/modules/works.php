751e71335148fec0c67566874669b2a7e3734dcf
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2023-04-12 21:02:55 -0500

    Update text banner y works

--- tezlik/modules/works.php
+++ tezlik/modules/works.php
@@ -4,8 +4,8 @@
         <div class="row">
             <div class="col-md-8 offset-md-2">
                 <div class="section-title">
-                    <h2>¿Quieres ahorrar en la compra de tus materias primas?</h2>
-                    <h6>Obten herramientas para negociar con tus proveedores</h6>
+                    <h2>Encuentra el equilibrio perfecto para tu negocio con TezlikSoftware</h2>
+                    <h6>¿Estás cansado de trabajar duro sin ver los resultados que esperas en tu negocio? ¿Te gustaría tener un mayor control sobre tus costos y ganancias? Si es así, estás en el lugar correcto.</h6>
                 </div>
             </div>
         </div>
@@ -16,8 +16,8 @@
                     <!-- <div class='container'><span class='pulse-button'><i class="ti-cloud-down"></i></span></div> -->
                     <div class="works-icon"><span class="pulse-p"><i class="ti-money"></i></span></div>
                     <div class="works-info">
-                        <h3>Usa la regla del 80/20</h3>
-                        <p>El 20% de las materias primas que consumen el 80% del costo.</p>
+                        <h3>Fijación de precios basada en costos</h3>
+                        <p>ezlikSoftware te ayuda a establecer precios competitivos basados en los costos de producción reales, lo que te permite maximizar tus ganancias.</p>
                     </div>
                     <span class="work-count">1</span>
                 </div>
@@ -27,8 +27,8 @@
                     <div class="works-icon"><span class="pulse-g"><i class="ti-star"></i></span></div>
                     <!-- <div class="works-icon icon-bg-2"><i class="ti-pulse"></i></div> -->
                     <div class="works-info">
-                        <h3>Simula cuanto te ahorras</h3>
-                        <p>Crea posibles escenarios de descuentos</p>
+                        <h3>Análisis de materia prima</h3>
+                        <p>Nuestra plataforma analiza la materia prima utilizada en tu proceso de producción y te ayuda a encontrar mejores precios en la negociación con proveedores.</p>
                     </div>
                     <span class="work-count">2</span>
                     <!--  <div class="works-info-bottom"><i class="ti-angle-double-down"></i></div> -->
@@ -39,8 +39,8 @@
                     <div class="works-icon"><span class="pulse-o"><i class="ti-control-shuffle"></i></span></div>
                     <!-- <div class="works-icon icon-bg-3"><i class="ti-face-smile"></i></div> -->
                     <div class="works-info">
-                        <h3>Conoce tu ahorro Mensual</h3>
-                        <p>El ahorro mensual te da tu punto de partida</p>
+                        <h3>Puntos de equilibrio</h3>
+                        <p>Con TezlikSoftware, puedes encontrar el punto de equilibrio de tu producción, lo que te permitirá saber cuánto debes vender para cubrir los costos y comenzar a generar ganancias.</p>
                     </div>
                     <span class="work-count">3</span>
                 </div>
@@ -50,8 +50,8 @@
                     <div class="works-icon"><span class="pulse-r"><i class="ti-stats-up"></i></span></div>
                     <!-- <div class="works-icon icon-bg-3"><i class="ti-face-smile"></i></div> -->
                     <div class="works-info">
-                        <h3>Conoce tu ahorro Anual</h3>
-                        <p>Proyecta cuanto puedes ahorrar</p>
+                        <h3>Mejora de la rentabilidad</h3>
+                        <p>Al utilizar nuestra plataforma para la fijación de precios, análisis de materia prima, economías de escala y puntos de equilibrio, puedes mejorar significativamente la rentabilidad de tu empresa manufacturera</p>
                     </div>
                     <span class="work-count">4</span>
                 </div>
