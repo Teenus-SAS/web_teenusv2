651f076cc8c80cff4cac8994fc986812a3ad3f03
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2023-04-23 11:00:52 -0500

    changes

--- tezlik/modules/pricing.php
+++ tezlik/modules/pricing.php
@@ -34,9 +34,9 @@
                         </ul>
                     </div>
                 </div>
-                <div class="pricing-footer">
-                    <a href="#contact" class="button btnPlan js-scroll" id="Premium">Solicita tu Demostración Gratuita</a>
-                </div>
+            </div>
+            <div class="pricing-footer">
+                <a href="#contact" class="button btnPlan js-scroll" id="Premium">Solicita tu Demostración Gratuita</a>
             </div>
             <!-- pricing item end -->
             <!-- pricing item -->
