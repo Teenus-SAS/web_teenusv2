fe0bfe2ac7cee84672043b713853565ccf50c958
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2023-04-23 13:15:10 -0500

    creation FAQs

--- tezlik/modules/faq.php
+++ tezlik/modules/faq.php
@@ -4,51 +4,43 @@
         <div class="row">
             <div class="col-md-12 section-title">
                 <h6 class="small-title">FAQs</h6>
-                <h2>Frequently Asked Questions</h2>
+                <h2>Preguntas Frecuentes</h2>
             </div>
             <div class="col-lg-6 col-md-12 faq-item first-faq-box">
                 <div class="accordion accordion-1">
                     <div class="accordion-item-1">
                         <div class="title">
-                            <h6><span>01)</span> Cómo puedo descargar tezlik?</h6>
+                            <h6><span>01)</span>¿Qué beneficios tiene usar TezlikSoftware para mi negocio?</h6>
                         </div>
                         <div class="accordion-info-1">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud consectetur adipiscing elit, sed do eiusmod tempor.
-                            </p>
+                            <p>Usar TezlikSoftware te ayuda a optimizar tus costos de producción y maximizar tus ganancias, al establecer precios competitivos basados en los costos reales de tu proceso. Además, te permite tener un mayor control sobre tu materia prima, tus puntos de equilibrio y tus economías de escala, lo que mejora la rentabilidad de tu empresa.</p>
                         </div>
                     </div>
 
                     <div class="accordion-item-1">
                         <div class="title">
-                            <h6><span>02)</span> Is the App support PayPall?</h6>
+                            <h6><span>02)</span>¿Cómo puedo registrarme y acceder a la plataforma?</h6>
                         </div>
                         <div class="accordion-info-1">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud consectetur adipiscing elit, sed do eiusmod tempor.
-                            </p>
+                            <p>Para registrarte y acceder a la plataforma, debes ingresar a https://www.teenus.com.co/tezliksoftware y en el formulario de contactenos ingresar tus datos personales y los de tu empresa. Luego, recibirás un correo electrónico de confirmación con tu usuario y contraseña. Con estos datos, podrás iniciar sesión en la plataforma y empezar a usar la solución.</p>
                         </div>
                     </div>
 
                     <div class="accordion-item-1">
                         <div class="title">
-                            <h6><span>03)</span> What is an Native application?</h6>
+                            <h6><span>03)</span>¿Qué tipo de información necesito ingresar para usar la solución?</h6>
                         </div>
                         <div class="accordion-info-1">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud consectetur adipiscing elit, sed do eiusmod tempor.
-                            </p>
+                            <p>Para usar la solución, necesitas ingresar la información necesaria para calcular los costos de producción y fijar los precios de tus productos. Esta información incluye: materias primas, mano de obra, tiempos, costos indirectos, rentabilidad y todo lo que necesites. La plataforma te guiará paso a paso para ingresar esta información de forma fácil y rápida, además te daremos consultoria totalmente gratis</p>
                         </div>
                     </div>
 
                     <div class="accordion-item-1">
                         <div class="title">
-                            <h6><span>04)</span> How much does Web App Solutions cost?</h6>
+                            <h6><span>04)</span>¿Cómo puedo contactar al soporte técnico si tengo algún problema o duda?</h6>
                         </div>
                         <div class="accordion-info-1">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud consectetur adipiscing elit, sed do eiusmod tempor.
-                            </p>
+                            <p>Si tienes algún problema o duda con la plataforma, puedes contactar al soporte técnico a través del correo electrónico soporte@teenus.com.co o del número de WhatsApp +(57)3002983150. Te atenderemos lo más pronto posible y te ayudaremos a resolver cualquier inconveniente que tengas.</p>
                         </div>
                     </div>
                 </div>
@@ -57,12 +49,11 @@
                 <div class="accordion accordion-2">
                     <div class="accordion-item-2">
                         <div class="title">
-                            <h6><span>05)</span> What is Web App Solutions?</h6>
+                            <h6><span>05)</span>¿Cómo puedo actualizar o cancelar mi plan?</h6>
                         </div>
                         <div class="accordion-info-2">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud consectetur adipiscing elit, sed do eiusmod tempor.
-                            </p>
+                            <p>Si quieres actualizar o cancelar tu plan, puedes hacerlo escribiendo al WhatsApp +(57)3002983150. Allí podrás ver las opciones disponibles para cambiar o dar de baja tu plan. Recuerda que si cancelas tu plan, perderás el acceso a la plataforma y a toda la información que hayas ingresado.</p>
+                            <!-- <p>Si quieres actualizar o cancelar tu plan, puedes hacerlo desde tu perfil en la plataforma. Allí podrás ver las opciones disponibles para cambiar o dar de baja tu plan. Recuerda que si cancelas tu plan, perderás el acceso a la plataforma y a toda la información que hayas ingresado.</p> -->
                         </div>
                     </div>
 
