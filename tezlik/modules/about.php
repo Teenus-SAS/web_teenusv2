d799e2f4385ba25d60b36b53254b8a539a1282a6
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2023-04-23 12:43:45 -0500

    FIX TEXT use tezxlik

--- tezlik/modules/about.php
+++ tezlik/modules/about.php
@@ -5,7 +5,7 @@
 
             <div class="col-lg-6 col-md-12">
                 <div class="about-info">
-                    <h6 class="small-title">¡TezlikSoftware es muy fácil de usar!. Simplemente ingresa la información necesaria</h6>
+                    <h6 class="small-title">¡TezlikSoftware es muy fácil de usar!. Simplemente ingresa la información necesaria y sigue estos pasos:</h6>
                     <h2>Cómo funciona</h2>
                     <div class="row d-flex align-items-center">
                         <div class="col-lg-12 col-md-12 col-sm-12 features-item-box">
@@ -13,7 +13,7 @@
                                 <!-- <div class="features-icon pulse-p"></div> -->
                                 <div class="features-icon" style="font-size:xx-large;color:crimson">1</div>
                                 <div class="features-info">
-                                    <span>Registra todos los costos de producción (Materias primas, mano de obra, tiempos, costos indirectos, rentabilidad y todo lo que necesites).</span>
+                                    <span>Registra todos los costos de producción (Materias primas, mano de obra, tiempos, costos indirectos, rentabilidad y todo lo que necesites) en nuestra plataforma.</span>
                                 </div>
                             </div>
                         </div>
@@ -23,7 +23,7 @@
                                 <!-- <div class="features-icon pulse-r"></div> -->
                                 <div class="features-icon" style="font-size:xx-large;color:crimson">2</div>
                                 <div class="features-info">
-                                    <span>TezlikSoftware de manera rápida y sencilla te calcula exactamente cuánto te cuesta producir cada unidad de tus productos.</span>
+                                    <span>TezlikSoftware te calcula de forma rápida y sencilla el costo exacto de producir cada unidad de tus productos.</span>
                                 </div>
                             </div>
                         </div>
@@ -32,7 +32,7 @@
                                 <!-- <div class="features-icon pulse-b"></div> -->
                                 <div class="features-icon" style="font-size:xx-large;color:darkorange">3</div>
                                 <div class="features-info">
-                                    <span>Toma decisiones informadas basadas en datos precisos y confiables gracias a nuestra solución.</span>
+                                    <span>Toma decisiones estratégicas basadas en datos precisos y confiables que te ofrece nuestra solución.</span>
                                 </div>
                             </div>
                         </div>
@@ -41,7 +41,7 @@
                                 <!-- <div class="features-icon pulse-o"></div> -->
                                 <div class="features-icon" style="font-size:xx-large;color:crimson">4</div>
                                 <div class="features-info">
-                                    <span>La solución te proporcionará un desglose detallado de los costos de producción y te permitirá fijar precios basados en los costos reales, asegurando que tus productos sean rentables y competitivos.</span>
+                                    <span>La solución te proporciona un desglose detallado de los costos de producción y te facilita fijar precios competitivos y rentables para tus productos.</span>
                                 </div>
                             </div>
                         </div>
