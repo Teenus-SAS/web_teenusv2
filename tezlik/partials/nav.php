5ffb4a4edca9076b635e8709154dd742530af7e2
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2023-07-22 14:40:20 -0500

    menu blog activated

--- tezlik/partials/nav.php
+++ tezlik/partials/nav.php
@@ -24,7 +24,7 @@
                 <!-- <li class="nav-item"><a class="nav-link js-scroll" href="#screenshort">Interfaces</a></li> -->
                 <li class="nav-item"><a class="nav-link js-scroll" href="#price">Precios</a></li>
                 <li class="nav-item"><a class="nav-link js-scroll" href="#video">Beneficios</a></li>
-                <li class="nav-item"><a class="nav-link js-scroll mr-5" href="/ebooks">Ebooks</a></li>
+                <!-- <li class="nav-item"><a class="nav-link js-scroll mr-5" href="/ebooks">Ebooks</a></li> -->
                 <li class="nav-item"><a href="#javascript:;" target="_blank" onclick="window.location.href='https://wa.me/573002983150?text=¡Hola! Me gustaría ver un Demo'" class="button btnPlan buttonContact js-scroll apps-store-btn-1" id="contactUs" style="margin-top:0px;border-radius: 10px;">Contactanos</a></li>
                 <li class="nav-item"><a href="#openModal" class="button apps-store-btn-1" id="registerTezlik" style="margin-top:0px;border-radius: 10px;">Registrate</a></li>
                 <li class="nav-item"><a href="#javascript:;" target="_blank" onclick="window.location.href='https://demo.tezliksoftware.com.co'" class="button btnPlan buttonLogin js-scroll apps-store-btn-1" id="login" style="margin-top:0px;border-radius: 10px;">Ingresar</a></li>
