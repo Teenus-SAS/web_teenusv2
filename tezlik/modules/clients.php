6d6496a617744cdf02fc861314599d70c60702ab
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2023-09-22 20:58:37 -0500

    change carrousel

--- tezlik/modules/clients.php
+++ tezlik/modules/clients.php
@@ -1,16 +1,44 @@
 <section id="price" class="price-area section-padding">
-    <div class="container">
+    <div class="row">
+        <div class="col-md-12 section-title">
+            <h6 class="small-title">Potencia tu empresa</h6>
+            <h2>ALGUNOS DE NUESTROS CLIENTES</h2>
+            <div class="slider mt-5">
+                <div class="slide-track">
+                    <div class="slide mr-3"></div>
+                    <div class="slide mr-3"></div>
+                    <div class="slide mr-3"></div>
+                    <div class="slide mr-3"></div>
+                    <div class="slide mr-3"></div>
+                    <div class="slide mr-3"></div>
+                    <div class="slide mr-3"></div>
+                    <div class="slide mr-3"></div>
+                    <div class="slide mr-3"></div>
+                    <div class="slide mr-3"></div>
+                    <div class="slide mr-3"></div>
+                    <div class="slide mr-3"></div>
+                    <div class="slide mr-3"></div>
+                    <div class="slide mr-3"></div>
+                    <div class="slide mr-3"></div>
+                    <div class="slide mr-3"></div>
+                    <div class="slide mr-3"></div>
+                    <div class="slide mr-3"></div>
+                </div>
+            </div>
+                
+        </div>
+    </div>
+    <!-- <div class="container">
         <div class="row">
             <div class="col-md-12 section-title">
                 <h6 class="small-title">Potencia tu empresa</h6>
                 <h2>SOLO ALGUNOS DE NUESTROS CLIENTES</h2>
-                <div class="containerCustomers carrousel">
+                <div class="carrousel">
                     <div class="card">
                         <div class="imgBx">
                             <img src="/tezlik/assets/img/clients/Proyecformas.png" alt="proyecformas" style="height: 90%" ;>
                         </div>
                         <div class="content">
-                            <!-- <h2>Proyecformas</h2> -->
                             <p>Diseño, fabricación y suministro mobiliario para el sector de la salud (farmacias, hospitales) el sector de oficinas y muebles para exhibición</p>
                         </div>
                     </div>
@@ -19,8 +47,7 @@
                             <img src="/tezlik/assets/img/clients/Blomer.png" alt="blomer" style="height: 90%">
                         </div>
                         <div class="content">
-                            <!-- <h2>Blomer</h2> -->
-                            <p>Cepillos profesionales para el cabello.</p>
+                                                        <p>Cepillos profesionales para el cabello.</p>
                         </div>
                     </div>
                     <div class="card">
@@ -28,7 +55,7 @@
                             <img src="/tezlik/assets/img/clients/artepack.png" alt="artepack" style="height: 90%">
                         </div>
                         <div class="content">
-                            <!-- <h2>Artepack</h2> -->
+                          
                             <p>Ofrecemos moldes y empaques de papel biodegradable con diseños personalizables</p>
                         </div>
                     </div>
@@ -37,7 +64,7 @@
                             <img src="/tezlik/assets/img/clients/gblab.png" alt="genericos de belleza" style="height: 90%">
                         </div>
                         <div class="content">
-                            <!-- <h2>Artepack</h2> -->
+                            
                             <p>Laboratorio para crear productos para el cabello en Colombia. Expertos en maquila</p>
                         </div>
                     </div>
@@ -46,7 +73,7 @@
                             <img src="/tezlik/assets/img/clients/micromotores.png" alt="micromotores" style="top:50px;position:relative">
                         </div>
                         <div class="content">
-                            <!-- <h2>Artepack</h2> -->
+                            
                             <p>Diseño y desarrollo de soluciones integrales, instalación y servicio en sistemas electrónicos especializados en seguridad, accesorios y componentes electrónicos para los sectores automotriz, residencial e industrial.</p>
                         </div>
                     </div>
@@ -55,12 +82,13 @@
                             <img src="/tezlik/assets/img/clients/siloscem.png" alt="silos cem" style="top:50px;position:relative">
                         </div>
                         <div class="content">
-                            <!-- <h2>Artepack</h2> -->
+                            
                             <p>Nos dedicamos a la fabricación de Equipos metalmecanicos como: Silos Corrugados Galvanizados, Elevadores de Cangilones, Bandas Transportadoras, Valvulas Dosificadoras, etc </p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
+
 </section>
