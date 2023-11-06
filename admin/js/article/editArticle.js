a977397aa9dee4109ab8fdf94ed430948b542346
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2023-11-06 17:01:02 -0500

    update edit

--- admin/js/article/editArticle.js
+++ admin/js/article/editArticle.js
@@ -1,47 +1,47 @@
 $(document).ready(function () {
-  localStorage.removeItem('id_article');
+  localStorage.removeItem("id_article");
 
   /* Modificar Fecha de publicación */
   updatePublication = (id_article, publication_date) => {
-   /*  var date = new Date();
+    var date = new Date();
     var dateString;
     date.setDate(date.getDate());
     dateString =
       date.getFullYear() +
-      '-' +
-      ('0' + (date.getMonth() + 1)).slice(-2) +
-      '-' +
-      ('0' + date.getDate()).slice(-2); */
+      "-" +
+      ("0" + (date.getMonth() + 1)).slice(-2) +
+      "-" +
+      ("0" + date.getDate()).slice(-2);
 
     bootbox.confirm({
-      title: 'Modificación de Articulo',
+      title: "Modificación de Articulo",
       message: `<p>Ingrese fecha de publicación:</p>
             <input id="publicationDate" class="bootbox-input bootbox-input-date form-control" autocomplete="off" type="date" min="${dateString}" value="${publication_date}">`,
       buttons: {
         confirm: {
-          label: 'Guardar',
-          className: 'btn-success',
+          label: "Guardar",
+          className: "btn-success",
         },
         cancel: {
-          label: 'Cancelar',
-          className: 'btn-danger',
+          label: "Cancelar",
+          className: "btn-danger",
         },
       },
       callback: function (result) {
         if (result == true) {
-          publication_date = $('#publicationDate').val();
+          publication_date = $("#publicationDate").val();
 
           data = {};
-          data['publicationDate'] = publication_date;
-          data['idArticle'] = id_article;
+          data["publicationDate"] = publication_date;
+          data["idArticle"] = id_article;
 
           $.post(
-            '/api/updatePublication',
+            "/api/updatePublication",
             data,
             function (data, textStatus, jqXHR) {
               message(data);
               setTimeout(() => {
-                location.href = '/admin/blogs-detalles';
+                location.href = "/admin/blogs-detalles";
               }, 2000);
             }
           );
@@ -52,19 +52,19 @@ $(document).ready(function () {
 
   /* Visualizar Articulo */
   viewArticle = (id) => {
-    localStorage.setItem('id_article', id);
-    window.open('/articulo', '_blank');
+    localStorage.setItem("id_article", id);
+    window.open("/articulo", "_blank");
   };
 
   /* Actualizar Articulos */
-  $(document).on('click', '.updateArticles', function (e) {
+  $(document).on("click", ".updateArticles", function (e) {
     idArticle = this.id;
-    sessionStorage.setItem('id_article', idArticle);
+    sessionStorage.setItem("id_article", idArticle);
 
     let row = $(this).parent().parent()[0];
     let data = tblArticles.fnGetData(row);
 
-    sessionStorage.setItem('data', JSON.stringify(data));
+    sessionStorage.setItem("data", JSON.stringify(data));
   });
 
   /* Eliminar Articulos */
@@ -76,17 +76,17 @@ $(document).ready(function () {
     let idArticle = data.id_article;
 
     bootbox.confirm({
-      title: 'Eliminar',
+      title: "Eliminar",
       message:
-        'Está seguro de eliminar este articulo? Esta acción no se puede reversar.',
+        "Está seguro de eliminar este articulo? Esta acción no se puede reversar.",
       buttons: {
         confirm: {
-          label: 'Si',
-          className: 'btn-success',
+          label: "Si",
+          className: "btn-success",
         },
         cancel: {
-          label: 'No',
-          className: 'btn-danger',
+          label: "No",
+          className: "btn-danger",
         },
       },
       callback: function (result) {
@@ -97,7 +97,7 @@ $(document).ready(function () {
               message(data);
 
               setTimeout(() => {
-                location.href = '/admin/blogs-detalles';
+                location.href = "/admin/blogs-detalles";
               }, 2000);
             }
           );
