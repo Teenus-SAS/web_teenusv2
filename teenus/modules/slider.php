8fb23771145523becb4e326d73e776f0e46be999
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2023-11-30 16:15:55 -0500

    change text banner

--- teenus/modules/slider.php
+++ teenus/modules/slider.php
@@ -7,12 +7,11 @@
                         <div class="row">
                             <div class="col-lg-8 col-md-12">
                                 <div class="caption">
-                                    <h1>¿Tu empresa no logra el crecimiento deseado a pesar de tus esfuerzos?</h1>
-                                    <!-- <h2>¡No permitas que tu precio espante a tus clientes!</h2> -->
-                                    <h5 class="mb-3"><!-- Déjame contarte que existe una solución…<br><br> -->
-                                        Podrías estar enfrentando desafíos como precios inadecuados y baja rentabilidad. ¿Deseas cambiar esta situación? Si tu respuesta es un rotundo "Sí", es fundamental gestionar adecuadamente los costos, establecer precios adecuados y agilizar el proceso de cotizar en solo 5 minutos. ¿Pero cómo hacerlo sin cometer errores? 
+                                    <h1>¿No logras calcular los costos de producción de manera precisa?</h1>
+                                    <!-- <h5 class="mb-3">
+                                        Podrías estar enfrentando desafíos como precios inadecuados y baja rentabilidad. ¿Deseas cambiar esta situación? Si tu respuesta es un rotundo "Sí", es fundamental gestionar adecuadamente los costos, establecer precios adecuados y agilizar el proceso de cotizar en solo 5 minutos. ¿Pero cómo hacerlo sin cometer errores?
                                         <a href="/tezliksoftware" style="color:yellow">Lee más<span></span></a>
-                                    </h5>
+                                    </h5> -->
                                     <div class="banner-btn home-slider-btn">
                                         <a href="/tezliksoftware" class="default-btn-one">Conoce más<span></span></a>
                                         <a class="default-btn" href="#contact">Contáctanos Ya<span></span></a>
@@ -29,9 +28,8 @@
                         <div class="row">
                             <div class="col-lg-8 col-md-12">
                                 <div class="caption">
-                                    <h1>¿Cotizar en 5 minutos?</h1> <br>
-                                    <!-- <h2>¡El problema no es tu producto es el precio!</h2> -->
-                                    <h5 class="mb-3"><a href="/tezlik" style="color:yellow">¿Pero cómo lograrlo? <span></span></a>Al alcanzar esta velocidad, podrías aumentar significativamente la probabilidad de cerrar tus ventas en un 25%. La capacidad de cotizar en 5 minutos es un valioso diferenciador en un mercado competitivo. Recuerda que la eficiencia y la calidad en el proceso de cotización son fundamentales para construir relaciones sólidas y cerrar ventas exitosas.</h5>
+                                    <h1>Te falta precisión en tus cotizaciones: Elimina la incertidumbre al proporcionar cálculos precisos de costos.</h1> <br>
+                                    <!-- <h5 class="mb-3"><a href="/tezlik" style="color:yellow">¿Pero cómo lograrlo? <span></span></a>Al alcanzar esta velocidad, podrías aumentar significativamente la probabilidad de cerrar tus ventas en un 25%. La capacidad de cotizar en 5 minutos es un valioso diferenciador en un mercado competitivo. Recuerda que la eficiencia y la calidad en el proceso de cotización son fundamentales para construir relaciones sólidas y cerrar ventas exitosas.</h5> -->
                                     <div class="banner-btn home-slider-btn">
                                         <a href="/tezliksoftware" class="default-btn-one">Conoce más<span></span></a>
                                         <a class="default-btn" href="#contact">Contactanos Ya<span></span></a>
@@ -48,9 +46,8 @@
                         <div class="row">
                             <div class="col-lg-8 col-md-12">
                                 <div class="caption">
-                                    <h1>¡Impulsa el crecimiento de tu Fabrica con Desarrollo de Software Personalizado!</h1> <br>
-                                    <!-- <h2>desde casa o empresa y sin necesidad de tener conocimientos contables, potencializa tu negocio para hacerlo altamente rentable en menos de 10 días </h2> -->
-                                    <h5 class="mb-3"><a href="/tezlik" style="color:yellow">¿Para qué lograr un rendimiento operativo excepcional que impulse tu rentabilidad?<span></span></a> Tu fabrica se encuentra en un sector altamente competitivo y mantenerse a la vanguardia tecnológica es fundamental para el éxito empresarial. El desarrollo de software personalizado se ha convertido en la herramienta más poderosa para que transformes tu fabrica y sea más competitiva. ¡No te quedes rezagado, tu competencia ya lo esta haciendo. Atrévete a innovar y alcanza el éxito con soluciones de software personalizadas para tu fabrica!</h5>
+                                    <h1>Tienes dificultades para adaptarte a cambios en los costos: Adquiere flexibilidad para ajustarse a variables del mercado rápidamente.</h1> <br>
+                                    <!-- <h5 class="mb-3"><a href="/tezlik" style="color:yellow">¿Para qué lograr un rendimiento operativo excepcional que impulse tu rentabilidad?<span></span></a> Tu fabrica se encuentra en un sector altamente competitivo y mantenerse a la vanguardia tecnológica es fundamental para el éxito empresarial. El desarrollo de software personalizado se ha convertido en la herramienta más poderosa para que transformes tu fabrica y sea más competitiva. ¡No te quedes rezagado, tu competencia ya lo esta haciendo. Atrévete a innovar y alcanza el éxito con soluciones de software personalizadas para tu fabrica!</h5> -->
                                     <div class="banner-btn home-slider-btn">
                                         <a href="/tezliksoftware" class="default-btn-one">Conoce más<span></span></a>
                                         <a class="default-btn" href="#contact">Contactanos Ya<span></span></a>
