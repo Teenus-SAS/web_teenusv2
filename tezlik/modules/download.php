e71162873a8220f1a3c5a62b56ac68525505ac81
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2023-11-30 16:09:18 -0500

    Change Text

--- tezlik/modules/download.php
+++ tezlik/modules/download.php
@@ -4,8 +4,8 @@
         <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12">
                 <div class="download-app-text text-center">
-                    <h2 class="text-uppercase">TEZLIKSOFTWARE ES LA SOLUCIÓN MÁS FÁCIL Y EFECTIVA PARA COSTEAR Y FIJAR PRECIOS…<br>
-                        <br>NO IMPORTA SI NO TIENES EXPERIENCIA EN CONTABILIDAD O TECNOLOGÍA, NOSOTROS TE GUIAMOS EN CADA PASO PARA QUE PUEDAS AUMENTAR TUS GANANCIAS CON NUESTRA PLATAFORMA
+                    <h2 class="text-uppercase">TEZLIKSOFTWARE ES LA SOLUCIÓN MÁS FÁCIL, EFECTIVA Y RÁPIDA PARA COTIZAR BASADO EN LOS COSTOS DE PRODUCCIÓN…<br>
+                        <!-- <br>NO IMPORTA SI NO TIENES EXPERIENCIA EN CONTABILIDAD O TECNOLOGÍA, NOSOTROS TE GUIAMOS EN CADA PASO PARA QUE PUEDAS AUMENTAR TUS GANANCIAS CON NUESTRA PLATAFORMA -->
                     </h2>
                     <!-- <a href="#javascript:;" onclick="window.location.href='https://wa.me/573002983150?text=¡Hola! Me gustaría ver un Demo'" class="button btnPlan js-scroll apps-store-btn-1" id="emprendedor"> -->
                     <a href="#openModal" class="button apps-store-btn-1" id="registerTezlik" style="margin-top:0px;border-radius: 10px;">¡Registrate Gratis!</a>
