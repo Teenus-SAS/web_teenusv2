a8d530d691e83297ba8098dc80382499eb132158
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2024-01-25 08:52:25 -0500

    change propuesta de valor

--- tezlik/modules/works.php
+++ tezlik/modules/works.php
@@ -5,7 +5,7 @@
             <div class="col-md-8 offset-md-2">
                 <div class="section-title">
                     <h2>Mejora la competitividad de tu fábrica con cotizaciones precisas.</h2>
-                    <h6 class="mt-3"><b></b>Optimiza tus procesos de cotización y gestión de costos de producción.</h6>
+                    <h6 class="mt-3"><b></b>Optimiza tus procesos de cotización basado en tus costos de producción.</h6>
                 </div>
             </div>
         </div>
@@ -15,8 +15,8 @@
                     <!-- <div class='container'><span class='pulse-button'><i class="ti-cloud-down"></i></span></div> -->
                     <div class="works-icon"><span class="pulse-p"><i class="ti-money"></i></span></div>
                     <div class="works-info">
-                        <h3>Optimiza tus costos de producción</h3>
-                        <p>Aumenta la rentabilidad de tu fábrica al identificar oportunidades de mejora.</p>
+                        <h3>Agiliza tu Proceso de Cotización.</h3>
+                        <p>Con TezlikSoftware generas y envias cotizaciones rápidamente. Esto se traduce en una toma de decisiones más veloz por parte de tus clientes, creando mas probabilidades de compra y mejorando su satisfacción al reducir los tiempos de espera.</p>
                     </div>
                     <span class="work-count">1</span>
                 </div>
@@ -26,8 +26,8 @@
                     <div class="works-icon"><span class="pulse-g"><i class="ti-star"></i></span></div>
                     <!-- <div class="works-icon icon-bg-2"><i class="ti-pulse"></i></div> -->
                     <div class="works-info">
-                        <h3>Aumenta la eficiencia operativa de tu fábrica </h3>
-                        <p>Automatiza procesos para ahorrar tiempo y reducir errores.</p>
+                        <h3>Elimina Errores en tus Cotizaciones</h3>
+                        <p>Con TezlikSoftware reduces la necesidad de retrabajos, correcciones y ajustes. Esto no solo ahorra tiempo y recursos, sino que también contribuye a la mejora de la calidad de tu producto final.</p>
                     </div>
                     <span class="work-count">2</span>
                     <!--  <div class="works-info-bottom"><i class="ti-angle-double-down"></i></div> -->
@@ -38,8 +38,8 @@
                     <div class="works-icon"><span class="pulse-o"><i class="ti-control-shuffle"></i></span></div>
                     <!-- <div class="works-icon icon-bg-3"><i class="ti-face-smile"></i></div> -->
                     <div class="works-info">
-                        <h3>Adaptate a cambios en el mercado rápidamente</h3>
-                        <p>Facilita ajustes rápidos a fluctuaciones de costos.</p>
+                        <h3>Exactitud en tus Cotizaciones</h3>
+                        <p>Con TezlikSoftware utilizas algoritmos y herramientas avanzadas para garantizar la precisión en tus cotizaciones. Esto disminuye los errores humanos, asegurando que tus cotizaciones reflejen exactamente los costos asociados, evitando pérdidas financieras y manteniendo la integridad del proceso.</p>
                     </div>
                     <span class="work-count">3</span>
                 </div>
@@ -49,8 +49,8 @@
                     <div class="works-icon"><span class="pulse-r"><i class="ti-stats-up"></i></span></div>
                     <!-- <div class="works-icon icon-bg-3"><i class="ti-face-smile"></i></div> -->
                     <div class="works-info">
-                        <h3>Aumenta la competitividad de tus precios</h3>
-                        <p>Establece precios atractivos y rentables..</p>
+                        <h3>Facilidad de Uso</h3>
+                        <p>La interfaz de TezlikSoftware está diseñada para ser intuitiva y fácil de usar, lo que simplifica la gestión de cotizaciones incluso para aquellos usuarios no familiarizados con tecnologías avanzadas. Esto reduce la curva de aprendizaje y minimiza la probabilidad de errores.</p>
                     </div>
                     <span class="work-count">4</span>
                 </div>
