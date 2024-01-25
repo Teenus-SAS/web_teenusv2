182e9e1f974de0e222643f5d8c3a286e199e9620
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2024-01-25 10:23:06 -0500

    fix text

--- tezlik/modules/banner.php
+++ tezlik/modules/banner.php
@@ -5,12 +5,12 @@
             <div class="col-12 col-md-7 col-lg-6">
                 <div class="text-left home-content z-index position-relative">
                     <!-- <h1 class="text-uppercase" style="color:white">Costea y cotiza en 5 minutos o te devolvemos tu dinero</h1> -->
-                    <h5 style="color:white">La lentitud y falta de precisión en los cálculos de costos provocan pérdida de oportunidades de negocio al no poder enviar cotizaciones rápidas. <br>Cotiza rapidamente: ¡Conquista a tus clientes!</h5>
+                    <h4 style="color:white">La lentitud y falta de precisión en los cálculos de costos provocan pérdida de oportunidades de negocio al no poder enviar cotizaciones rápidas. <br><br>Cotiza rapidamente: ¡Conquista a tus clientes!</h4>
                     <div class="d-lg-none text-center z-index position-relative home-image">
                         <img src="/tezlik/assets/img/calcular-precio-producto.png" class="img-fluid" alt="" />
                     </div>
 
-                    <div class="pricing-footer">
+                    <div class="pricing-footer mt-5">
                         <!-- <a href="javascript:;" target="_blank" onclick="window.location.href='https://wa.me/573002983150?text=¡Hola! Me gustaría ver un Demo'" class="button btnPlan js-scroll" id="emprendedor">Registrate Gratis</a> -->
                         <a href="#openModal" class="button apps-store-btn-1" id="registerTezlik" style="margin-top:0px;border-radius: 10px;">Registrate Gratis</a>
                     </div>
