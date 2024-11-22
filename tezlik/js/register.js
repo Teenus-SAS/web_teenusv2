04e881402772ae55a7af591973d8358bde51683f
Daniel Ledesma <daniel.ledesma@teenus.com.co>
2024-11-22 17:42:22 -0500

    change

--- tezlik/js/register.js
+++ tezlik/js/register.js
@@ -1,19 +1,46 @@
 $(document).ready(function () {
-  /* Register */
-  $('#btnRegister').click(function (e) {
+  $('#registerTezlik').click(function (e) { 
     e.preventDefault();
 
-    let email = $('#emailUser').val();
+    $('#openModal').modal('show');    
+  });
 
-    if (email == '') {
+  $('.closeRegisterTezlik').click(function (e) { 
+    e.preventDefault();
+
+    $('#openModal').modal('hide');    
+  });
+
+  /* Register */
+  $('#btnRegisterTezlik').click(function (e) {
+    e.preventDefault();
+
+    let firstname = $('#firstname').val();
+    let lastname = $('#lastname').val();
+    let email = $('#email').val();
+    let employees = $('#employees').val();
+
+    if (
+      firstname == ''||
+      lastname == ''||
+      email == ''||
+      employees == ''
+    ) {
       toastr.error('Ingrese todos los campos');
       return false;
     }
 
-    fetch(`https://demo.tezliksoftware.com.co/api/newUserAndCompany/${email}`)
-      // fetch(`http://tezlikv3/api/newUserAndCompany/${email}`)
-      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
-      .then((resp) => {
+    let data = new FormData(formRegister);
+
+    $.ajax({
+      type: "POST",
+      url: 'http://tezlikv3/api/newUserAndCompany',
+      // url: 'https://demo.tezliksoftware.com.co/api/newUserAndCompany',
+      data: data,
+      contentType: false,
+      cache: false,
+      processData: false,
+      success: function (resp) {
         if (resp.success == true) {
           setTimeout(reloadPage, 5000);
 
@@ -21,11 +48,25 @@ $(document).ready(function () {
           console.log(resp.pass);
         } else if (resp.error == true) toastr.error(resp.message);
         else if (resp.info == true) toastr.info(resp.message);
-      })
+      }
+    });
 
-      .catch((err) => {
-        console.log(err);
-      });
+    // fetch(`https://demo.tezliksoftware.com.co/api/newUserAndCompany/${email}`)
+    //   // fetch(`http://tezlikv3/api/newUserAndCompany/${email}`)
+    //   .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+    //   .then((resp) => {
+    //     if (resp.success == true) {
+    //       setTimeout(reloadPage, 5000);
+
+    //       toastr.success(resp.message);
+    //       console.log(resp.pass);
+    //     } else if (resp.error == true) toastr.error(resp.message);
+    //     else if (resp.info == true) toastr.info(resp.message);
+    //   })
+
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
   });
 
   function reloadPage() {
--- tezlik/modals/modalRegister.php
+++ tezlik/modals/modalRegister.php
@@ -1,4 +1,4 @@
-<div id="openModal" class="modalDialog">
+<!-- <div id="openModal" class="modalDialog">
     <div class="modalReg">
         <a href="#close" title="Close" class="close">X</a>
         <div class="one mb-3">
@@ -10,6 +10,72 @@
         </div>
         <div>
             <button class="btn btn-primary ml-3" style="padding:15px" id="btnRegister">Obten TezlikSoftware</button>
+        </div>
+    </div>
+</div> -->
+<div class="modal fade" id="openModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Registrate <br>¡y comienza a usar TezlikSoftware en segundos!</h5>
+                <button type="button" class="closeRegisterTezlik" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="page-content-wrapper mt--45">
+                    <div class="container-fluid">
+                        <div class="vertical-app-tabs" id="rootwizard">
+                            <div class="col-md-12 col-lg-12 InputGroup">
+                                <form id="formRegister">
+                                    <div class="row mt-5">
+                                        <div class="col-12 col-lg-12 titlePayroll">
+                                            <label><b>Datos</b></label>
+                                        </div>
+                                        <div class="col-12 col-lg-6">
+                                            <div class="form-group floating-label enable-floating-label show-label">
+                                                <input type="text" class="form-control" name="nameUser" id="firstname">
+                                                <label for="firstname">Nombre<span class="text-danger">*</span></label>
+                                                <div class="validation-error d-none font-size-13">Requerido</div>
+                                            </div>
+                                        </div>
+                                        <div class="col-12 col-lg-6">
+                                            <div class="form-group floating-label enable-floating-label show-label">
+                                                <input type="text" class="form-control" name="lastnameUser" id="lastname">
+                                                <label for="lastname">Apellido<span class="text-danger">*</span></label>
+                                                <div class="validation-error d-none font-size-13">Requerido</div>
+                                            </div>
+                                        </div>
+                                        <div class="col-12 col-lg-6">
+                                            <div class="form-group floating-label enable-floating-label show-label">
+                                                <input type="email" class="form-control" name="emailUser" id="email">
+                                                <label for="email">Email<span class="text-danger">*</span></label>
+                                                <div class="validation-error d-none font-size-13">Requerido</div>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-12 col-lg-12 titlePayroll">
+                                            <label><b>Compañia</b></label>
+                                        </div>
+
+                                        <div class="col-12 col-lg-4">
+                                            <div class="form-group floating-label enable-floating-label show-label">
+                                                <input type="number" class="form-control" name="quantity_user" id="employees">
+                                                <label for="employees">No Empleados<span class="text-danger">*</span></label>
+                                                <div class="validation-error d-none font-size-13">Requerido</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary closeRegisterTezlik" data-bs-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-primary" id="btnRegisterTezlik">Crear</button>
+            </div>
         </div>
     </div>
 </div>
