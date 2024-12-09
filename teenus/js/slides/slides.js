1bb3d649ebe85006b57a667138fd2f0784e9d2fc
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2024-12-09 14:38:14 -0500

    update route

--- teenus/js/slides/slides.js
+++ teenus/js/slides/slides.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
   function loadProjects() {
     $.ajax({
-      url: "/api/paint-ponts",
+      url: "/api/paint-points",
       success: function (slides) {
         // Limpiar el contenedor
         slidesContainer.empty();
